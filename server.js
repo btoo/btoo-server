afcffa0e26dc8e71c382db7caa1daf5b3b2c4745
btoo <feihcsim824@gmail.com>
2016-09-10 15:18:05 -0400

    res

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(bodyParser.json());
 
 // allow requests from btoo.github.io
 app.use(function(req,res,next){
-	// req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	// res.setHeader('Access-Control-Allow-Origin','https://btoo.github.io');
 	res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
-	// res.setHeader('Content-Type','application/json;');
+	res.setHeader('Content-Type','application/json;');
 	next();
 });
 // app.all('/', function(req, res, next) {
